495724797d1154ac17bc7f10945032c18169517b
Damian Veltkamp <damian.veltkamp@deptagency.com>
2024-03-19 11:28:40 +0100

    foobaz

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,12 @@ import { cookies } from "next/headers";
 export default async function Home() {
   const ip = cookies().get('x-ip-testing')?.value;
   const country = cookies().get('x-country')?.value;
-  const countryBasedOnIP = await fetch(`https://api.iplocation.net/?ip=62.163.213.53`)
 
   return (
     <main className={styles.main}>
       Homepage
       <ul>
         <li>{ip}</li>
-        <li>Geo data gotten from api.iplocation api {countryBasedOnIP.json()}</li>
         <li>Country gotten from vercels geo {country}</li>
       </ul>
     </main>
