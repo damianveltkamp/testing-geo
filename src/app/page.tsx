7efa164a55b3d9c80177b66ea1d3cdd3f337a67f
Damian Veltkamp <damian.veltkamp@deptagency.com>
2024-03-19 11:21:42 +0100

    foo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@ import { cookies } from "next/headers";
 
 export default function Home() {
   const ip = cookies().get('x-ip-testing')?.value;
+  const country = cookies().get('x-country')?.value;
 
   return (
     <main className={styles.main}>
       Homepage
       <h1>      {ip}
       </h1>
+      <p>{country}</p>
     </main>
   );
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -5,5 +5,6 @@ export function middleware(request: NextRequest) {
   const response = NextResponse.next()
   const ip = request.headers.get('X-Forwarded-For')
   response.cookies.set('x-ip-testing', ip ?? '', { httpOnly: false });
+  response.cookies.set('x-country', request.geo?.country ?? '', { httpOnly: false });
   return response
 }
